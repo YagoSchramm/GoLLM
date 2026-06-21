9496c00ffc804b6fdbc8183cd9784d3337edab89
YagoSchramm <yrtoiu1515@gmail.com>
2026-06-21 18:32:59 -0300

    chore(): Added config model

--- model/config.go
+++ model/config.go
@@ -1,1 +1,32 @@
 package model
+
+type Config struct {
+	VocabSize  int
+	ContextLen int
+	EmbedDim   int
+	NumHeads   int
+	NumLayers  int
+	FFNDim     int
+}
+
+func (t *Config) TinyConfig(vocabSize int) *Config {
+	return &Config{
+		VocabSize:  vocabSize,
+		ContextLen: 64,
+		EmbedDim:   64,
+		NumHeads:   2,
+		NumLayers:  2,
+		FFNDim:     128,
+	}
+}
+
+func (t *Config) SmallConfig(vocabSize int) *Config {
+	return &Config{
+		VocabSize:  vocabSize,
+		ContextLen: 256,
+		EmbedDim:   128,
+		NumHeads:   4,
+		NumLayers:  4,
+		FFNDim:     512,
+	}
+}
